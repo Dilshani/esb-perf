64d71d41fc47179c2bf6f9a06e320aee812ac8b6
chamaraanu <chamaraanuradha@gmail.com>
2017-04-25 12:16:00 +0000

    fixes

--- bash/esb-perf-execution.sh
+++ bash/esb-perf-execution.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
 echo "Starting remote Tomcat"
-ssh -i chamaraa.pem ubuntu@$1 'export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/ubuntu/java/jdk1.8.0_121/bin;./tomcat/apache-tomcat-7.0.29/bin/catalina.sh start' &
+#ssh -i chamaraa.pem ubuntu@$1 'export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/ubuntu/java/jdk1.8.0_121/bin;./tomcat/apache-tomcat-7.0.29/bin/catalina.sh start' &
+ssh esbperfbackend "./start_tomcat.sh"
+sleep 10
 
 echo "Starting remote ESB Server!"
-ssh -i chamaraa.pem ubuntu@$2 'export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/ubuntu/java/jdk1.8.0_121/bin;./esb/wso2esb-5.0.0/bin/wso2server.sh start' & 
+#ssh -i chamaraa.pem ubuntu@$2 'export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/ubuntu/java/jdk1.8.0_121/bin;./esb/wso2esb-5.0.0/bin/wso2server.sh start' & 
+ssh esbperfesb "./start_ei.sh"
+sleep 10
 
 echo "Checking ESB Server Health"
-
 while true 
 do
-  RSP="$(curl -sL -w "%{http_code}\\n"  "http://$2:8280/services/Version" -o /dev/null)"
+  RSP="$(curl -sL -w "%{http_code}\\n"  "http://$1:8280/services/Version" -o /dev/null)"
   if [ "$RSP" == 200 ]
   then
       break
   fi
   echo "Server Up"
 done
+sleep 10
 
 echo "Jmeter script execution goes here!!!"
-/home/ubuntu/apache-jmeter-2.11/bin/jmeter -JConcurrency=$3 -JDuration=$4 -JHost=$2 -JService=$5 -JPayload=$6 -n -t ESB_Perf.jmx
+JVM_ARGS="-Xms2g -Xmx2g -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:$PWD/results/gclogs/jmeter_gc_$1_$2.log"
+/home/ubuntu/apache-jmeter-3.2/bin/jmeter.sh -JConcurrency=$2 -JDuration=$3 -JHost=$1 -JService=$4 -JPayload=$5 -n -t ESB_Perf.jmx -l $4_C_$2_T_$3_P_$5.jtl -e -o reports/$4_C_$2_T_$3_P_$5 
+ss -s > results/jmeter_ss_$4_C_$2_T_$3_P_$5.txt
+
 echo "Done"
+
 ssh -i chamaraa.pem ubuntu@192.168.48.168 'killall java'
+
+sleep 30
--- bash/iterations.sh
+++ bash/iterations.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./esb-perf-execution.sh 192.168.48.168 192.168.48.168 10 300 DirectProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 20 300 DirectProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 40 300 DirectProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 80 300 DirectProxy 5K_buyStocks.xml
