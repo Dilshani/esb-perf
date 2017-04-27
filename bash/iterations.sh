165e26deb9434cf65d8f7c07be97bf9aa24d1fb0
chamaraanu <chamaraanuradha@gmail.com>
2017-04-27 05:39:13 +0000

    More iterations added

--- bash/iterations.sh
+++ bash/iterations.sh
@@ -1,5 +1,93 @@
 #!/bin/bash
 
-./esb-perf-execution.sh 192.168.48.168 20 300 DirectProxy 5K_buyStocks.xml
-./esb-perf-execution.sh 192.168.48.168 40 300 DirectProxy 5K_buyStocks.xml
-./esb-perf-execution.sh 192.168.48.168 80 300 DirectProxy 5K_buyStocks.xml
+#DirectProxy
+./esb-perf-execution.sh 192.168.48.168 10 3600 DirectProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 50 3600 DirectProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 100 3600 DirectProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 500 3600 DirectProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1000 3600 DirectProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1500 3600 DirectProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2000 3600 DirectProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2500 3600 DirectProxy 1K_buyStocks.xml
+
+./esb-perf-execution.sh 192.168.48.168 10 3600 DirectProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 50 3600 DirectProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 100 3600 DirectProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 500 3600 DirectProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1000 3600 DirectProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1500 3600 DirectProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2000 3600 DirectProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2500 3600 DirectProxy 5K_buyStocks.xml
+
+./esb-perf-execution.sh 192.168.48.168 10 3600 DirectProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 50 3600 DirectProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 100 3600 DirectProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 500 3600 DirectProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1000 3600 DirectProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1500 3600 DirectProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2000 3600 DirectProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2500 3600 DirectProxy 10K_buyStocks.xml
+
+./esb-perf-execution.sh 192.168.48.168 10 3600 DirectProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 50 3600 DirectProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 100 3600 DirectProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 500 3600 DirectProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1000 3600 DirectProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1500 3600 DirectProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2000 3600 DirectProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2500 3600 DirectProxy 100K_buyStocks.xml
+
+./esb-perf-execution.sh 192.168.48.168 10 3600 DirectProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 50 3600 DirectProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 100 3600 DirectProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 500 3600 DirectProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1000 3600 DirectProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1500 3600 DirectProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2000 3600 DirectProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2500 3600 DirectProxy 500K_buyStocks.xml
+
+$CBRProxy
+./esb-perf-execution.sh 192.168.48.168 10 3600 CBRProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 50 3600 CBRProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 100 3600 CBRProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 500 3600 CBRProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1000 3600 CBRProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1500 3600 CBRProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2000 3600 CBRProxy 1K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2500 3600 CBRProxy 1K_buyStocks.xml
+
+./esb-perf-execution.sh 192.168.48.168 10 3600 CBRProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 50 3600 CBRProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 100 3600 CBRProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 500 3600 CBRProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1000 3600 CBRProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1500 3600 CBRProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2000 3600 CBRProxy 5K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2500 3600 CBRProxy 5K_buyStocks.xml
+
+./esb-perf-execution.sh 192.168.48.168 10 3600 CBRProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 50 3600 CBRProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 100 3600 CBRProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 500 3600 CBRProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1000 3600 CBRProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1500 3600 CBRProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2000 3600 CBRProxy 10K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2500 3600 CBRProxy 10K_buyStocks.xml
+
+./esb-perf-execution.sh 192.168.48.168 10 3600 CBRProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 50 3600 CBRProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 100 3600 CBRProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 500 3600 CBRProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1000 3600 CBRProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1500 3600 CBRProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2000 3600 CBRProxy 100K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2500 3600 CBRProxy 100K_buyStocks.xml
+
+./esb-perf-execution.sh 192.168.48.168 10 3600 CBRProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 50 3600 CBRProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 100 3600 CBRProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 500 3600 CBRProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1000 3600 CBRProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 1500 3600 CBRProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2000 3600 CBRProxy 500K_buyStocks.xml
+./esb-perf-execution.sh 192.168.48.168 2500 3600 CBRProxy 500K_buyStocks.xml
